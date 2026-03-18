e9ff22dae73852ce4a16db7a7e6ffa7c0d978b01
Muhammed Taha Ayan <tahaayan@gmail.com>
2026-03-19 00:10:15 +0300

    fix(ui): update resizable primitives and add React type deps
    Align the resizable component with current react-resizable-panels exports and add explicit React type packages to keep type resolution stable across the workspace.

--- libs/ui/src/components/ui/resizable.tsx
+++ libs/ui/src/components/ui/resizable.tsx
@@ -3,9 +3,9 @@ import * as ResizablePrimitive from 'react-resizable-panels'
 
 import { cn } from '../../lib/utils'
 
-function ResizablePanelGroup({ className, ...props }: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) {
+function ResizablePanelGroup({ className, ...props }: React.ComponentProps<typeof ResizablePrimitive.Group>) {
   return (
-    <ResizablePrimitive.PanelGroup
+    <ResizablePrimitive.Group
       className={cn('flex h-full w-full data-[panel-group-direction=vertical]:flex-col', className)}
       data-slot="resizable-panel-group"
       {...props}
@@ -21,11 +21,11 @@ function ResizableHandle({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
+}: React.ComponentProps<typeof ResizablePrimitive.Separator> & {
   withHandle?: boolean
 }) {
   return (
-    <ResizablePrimitive.PanelResizeHandle
+    <ResizablePrimitive.Separator
       className={cn(
         'relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90',
         className,
@@ -34,7 +34,7 @@ function ResizableHandle({
       {...props}
     >
       {withHandle && <div className="z-10 flex h-6 w-1 shrink-0 rounded-none bg-border" />}
-    </ResizablePrimitive.PanelResizeHandle>
+    </ResizablePrimitive.Separator>
   )
 }
 
